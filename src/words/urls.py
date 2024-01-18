3e2f5b7f497e693f2a57f9545b59a0b7ae03696b
Mikhail Postnikov <postnikovmu@mail.ru>
2024-01-18 23:17:00 +0300

    add add_word view and template

--- src/words/urls.py
+++ src/words/urls.py
@@ -2,8 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.hello),
     path('home/', views.hello),
     path('words_list/', views.show_words_list),
+    path('add_word/', views.add_word, name='add_word'),
+    path('', views.hello),
 ]
 
--- src/words/views.py
+++ src/words/views.py
@@ -1,3 +1,4 @@
+from django.http import HttpResponsePermanentRedirect
 from django.shortcuts import render
 from .services import read_from_file, add_to_file
 
@@ -9,8 +10,16 @@ def hello(request):
 
 def show_words_list(request):
     if request.method == 'GET':
-        words1 = ['test']
-        words2 = ['тест']
+        words1, words2 = read_from_file()
         words_list = zip(words1, words2)
         data = {"words_list": words_list}
         return render(request, 'words/words_list.html', context=data)
+
+
+def add_word(request):
+    if request.method == 'POST':
+        word1 = request.POST['word1']
+        word2 = request.POST['word2']
+        add_to_file(word1, word2)
+        return HttpResponsePermanentRedirect('/')
+    return render(request, 'words/add_word.html')
